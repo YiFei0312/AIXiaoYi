82500db76ee952f5365f9c4a7ed3ef23a9950d19
YiFei0312 <135489451+YiFei0312@users.noreply.github.com>
2024-07-03 21:12:01 +0800

    Update main.py
    增加唤醒词

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import os
 import time
-import Speech_Synthesizer
-import Speech_Recognition
+from speech import Speech_Recognition, Speech_Synthesizer, Speech_Waken
 import qwen
-import config
+from config import config
 
-if not os.path.exists('./config.ini'):
+
+
+# 生成配置文件
+if not os.path.exists('./config/config.ini'):
+    if not os.path.exists('./config'):
+        os.mkdir('./config')
     config.generate_config_file()
+# 读取配置文件
+print('正在读取配置文件...')
 config.read_config_file()
-Speech_Recognition.start()
 manager=qwen.BookkeepingManager()
+
 while True:
-    if Speech_Recognition.start_time is not None and time.time() - Speech_Recognition.start_time > 30:
-        Speech_Recognition.stop()
+    if Speech_Waken.speech_recognize_keyword_from_microphone():
+        Speech_Synthesizer.start('唉！我在！')
         Speech_Recognition.start()
-    print('等待语音输入...')
-    Speech_Recognition.send_audio_frame()   # 从流中读取音频数据，并将其发送给识别服务。
-    if Speech_Recognition.last_voice_time is not None and time.time() - Speech_Recognition.last_voice_time > 1:     # 判断停顿时间断句
-        print('记录下完整语音：', Speech_Recognition.latest_sentence)
-        if Speech_Recognition.last_voice_time is not None:
-            Speech_Recognition.stop()
-        Speech_Synthesizer.start(qwen.get_qwen_response_parallel(manager,Speech_Recognition.latest_sentence))     # 调用语音合成 合成从千问获取的回答
-        Speech_Recognition.start()
-        print('对话结束，程序重新开始...')
+        while True:
+            if Speech_Recognition.start_time is not None and time.time() - Speech_Recognition.start_time > 30:
+                Speech_Recognition.stop()
+                Speech_Recognition.start()
+            print('等待语音输入...')
+            Speech_Recognition.send_audio_frame()   # 从流中读取音频数据，并将其发送给识别服务。
+            if Speech_Recognition.last_voice_time is not None and time.time() - Speech_Recognition.last_voice_time > 1:     # 判断停顿时间断句
+                print('记录下完整语音：', Speech_Recognition.latest_sentence)
+                if Speech_Recognition.last_voice_time is not None:
+                    Speech_Recognition.stop()
+                Speech_Synthesizer.start(qwen.get_qwen_response_parallel(manager, Speech_Recognition.latest_sentence))     # 调用语音合成 合成从千问获取的回答
+                print('对话结束，程序重新开始...')
+                break
+
