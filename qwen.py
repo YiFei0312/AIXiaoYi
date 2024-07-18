15f2731fc13851d369fa3b2cfd173aa1c7e6b276
YiFei0312 <135489451+YiFei0312@users.noreply.github.com>
2024-07-18 11:21:18 +0800

    Add files via upload

--- qwen.py
+++ qwen.py
@@ -11,56 +11,6 @@ class BookkeepingManager:
     def __init__(self):
         self.ai_bookkeeping = [{'role': 'system', 'content': config.personality['default']}]
 
-    def get_qwen_response_serial(self, mess: str):  # 串行请求
-        try:
-            response = dashscope.Generation.call(model="qwen-max",
-                                                 messages=[  # type: ignore
-                                                     {'role': 'system',
-                                                      'content': '你是一个负责判断是否使用工具的助手'},
-                                                     {'role': 'user', 'content': mess}],
-                                                 tools=tools.tools,
-                                                 result_format='message')
-            if response.status_code == http.client.OK:
-                assistant_message = response.output.choices[0]['message']
-                if 'tool_calls' in response.output.choices[0].message:
-                    tool_name = response.output.choices[0].message.tool_calls[0]['function']['name']
-                    if tool_name in self.TOOL_MAP:
-                        tool_message = self.TOOL_MAP[tool_name]()
-                        self.add_conversation('user', mess)
-                        self.add_conversation(assistant_message['role'], tool_message)
-                        return tool_message
-                    elif tool_name in self.TOOL_MAP_1:
-                        description = \
-                            json.loads(response.output.choices[0].message.tool_calls[0]['function']['arguments'])[
-                                'description']
-                        tool_message = tools.draw_picture(description)
-                        self.add_conversation('user', mess)
-                        self.add_conversation(assistant_message['role'], tool_message)
-                        return tool_message
-                else:
-                    self.add_conversation('user', mess)
-                    response = dashscope.Generation.call(model="qwen-max",
-                                                         messages=self.ai_bookkeeping,  # type: ignore
-                                                         result_format='message')
-                    if response.status_code == http.client.OK:
-                        assistant_message = response.output.choices[0]['message']  # 成功，添加assistant回复
-                        print(assistant_message['content'])
-                        self.add_conversation(assistant_message['role'], assistant_message['content'])
-                        return assistant_message['content']
-                    else:
-                        print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
-                            response.request_id, response.status_code,
-                            response.code, response.message
-                        ))  # 失败，打印错误信息并回滚最后一条消息
-                        self.ai_bookkeeping = self.ai_bookkeeping[:-1]
-            else:
-                print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
-                    response.request_id, response.status_code,
-                    response.code, response.message
-                ))  # 失败，打印错误信息
-
-        except Exception as e:
-            print(f'Exception occurred: {e}')
 
     def clear_request(self):
         """清空对话记录"""
@@ -83,19 +33,25 @@ class BookkeepingManager:
                                              result_format='message')
         if response.status_code == http.client.OK:
             assistant_message = response.output.choices[0]['message']
-            tool = response.output.choices[0].message.tool_calls[0]['function']
-            tool_name = tool['name']
-            property_list = json.loads(tool['arguments'])
-            property_value = list(property_list.values())
-            tool_message = self.TOOL_MAP[tool_name](*property_value)
-            if tool_message is None:
-                tool_message = '工具调用成功。'
-            return [tool_message, assistant_message['role']]
+            if 'tool_calls' in response.output.choices[0].message:
+                print(assistant_message)
+                tool = response.output.choices[0].message.tool_calls[0]['function']
+                tool_name = tool['name']
+                property_list = json.loads(tool['arguments'])
+                property_value = list(property_list.values())
+                tool_message = self.TOOL_MAP[tool_name](*property_value)
+                if tool_message is None:
+                    tool_message = '工具调用成功。'
+                return [tool_message, assistant_message['role']]
+            else:
+                return
         else:
             print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
                 response.request_id, response.status_code,
                 response.code, response.message
             ))  # 失败，打印错误信息
+            tool_message = '请求工具失败'
+            return [tool_message, 'assistant']
 
     def get_response(self, mess: str):  # 多轮对话回复
         self.add_conversation('user', mess)
@@ -117,10 +73,11 @@ class BookkeepingManager:
         with ThreadPoolExecutor() as executor:
             ftool = executor.submit(self.get_tool_response, mess)
             future = executor.submit(self.get_response, mess)
-        if ftool.result() == None:
+        if ftool.result() is None:
             print(future.result())
             return future.result()
         else:
             self.ai_bookkeeping = self.ai_bookkeeping[:-1]
             self.add_conversation(ftool.result()[1], ftool.result()[0])
+            print(self.ai_bookkeeping)
             return ftool.result()[0]
